353cc233b2fe5028b5d728a2582bb56f1116731e
李家骏 <geometry2019@outlook.com>
2021-10-30 18:17:38 +0800

    又调了一下结构，数据的储存应该是差不多了，搞一下数据添加

--- main.cpp
+++ main.cpp
@@ -4,23 +4,5 @@
 using namespace std;
 
 int main(int, char**) {
-    auto c = stringstream();
-    c << "cnmd";
-    string a;
-    c >> a;
-    cout << a << endl;
-    c >> a;
-    c << "n";
-    a.clear();
-    cout << a << endl;
-    c >> a;
-    cout << a << endl;
-    c >> a;
-    cout << a << endl;
-    c >> a;
-    cout << a << endl;
-    c >> a;
-    cout << a << endl;
-    c >> a;
-    cout << a << endl;
+
 }
--- vui.creater.hpp
+++ vui.creater.hpp
@@ -3,6 +3,9 @@
 #include <concepts>
 #include <vector>
 #include <sstream>
+#include <ranges>
+
+
 namespace vui{
 #define SUCCEDD 0
 #define ERROR_TARGET_NOT_FOUND 1
@@ -16,11 +19,14 @@ namespace vui{
         Object,               
         Array,               //coming soon 
         };
+    
+    
     struct VUIBasicElement{
         VUITypes selfType=VUITypes::Object;
         virtual std::string OutSelf() =0;
 
     };
+    
     struct VUIStrElement:VUIBasicElement{
         std::string BODY;
         std::string OutSelf() override{
@@ -35,8 +41,10 @@ namespace vui{
             return std::to_string(BODY);
         }
     };
+    
     struct VUIObjectElement:VUIBasicElement{
         std::map<std::string, VUIBasicElement> BODY;
+        
         std::string OutSelf() override
         {
             std::stringstream ss("");
@@ -44,47 +52,73 @@ namespace vui{
             std::string temp;
             for (auto i : BODY)
             {
+
                 switch (i.second.selfType)
                 {
                 case VUITypes::Number:
                     ss << i.first << "{" << i.second.OutSelf() << "}";
-                    ss >> temp;
-                    rt.push_back(temp);
+                    rt.push_back(ss.str());
                     rt.clear();
+                    ss.str("");
                     ss.clear();
                     break;
                 case VUITypes::Object:
                     ss << i.first << "{" << i.second.OutSelf() << "}";
-                    ss >> temp;
-                    rt.push_back(temp);
-                    rt.clear();
+                    rt.push_back(ss.str());
+                    ss.str("");
                     ss.clear();
                     break;
                 case VUITypes::Array:
                     ss << i.first << "{" << i.second.OutSelf() << "}";
-                    ss >> temp;
-                    rt.push_back(temp);
-                    rt.clear();
+                    rt.push_back(ss.str());
+                    ss.str("");
                     ss.clear();
                     break;
                 case VUITypes::Str:
                     ss << i.first << "{\"" << i.second.OutSelf() << "\"}";
-                    ss >> temp;
-                    rt.push_back(temp);
-                    rt.clear();
+                    rt.push_back(ss.str());
+                    ss.str("");
                     ss.clear();
                     break;
                 default:
                     break;
                 }
             }
+            for (auto i: std::views::iota(0,rt.size())){
+                temp.append(rt.at(i));
+                if (i!=rt.size()-2){
+                    temp.append(",");
+                }
+            }
+            return temp;
+        }
+
+        /*Used to add a object as target's children*/
+        int AddElement(std::string HEAD,VUIObjectElement source){
+            this->BODY[HEAD] = source;
+            return SUCCEDD;
+        }
+
+        /*Used to add a number as target's children*/
+        template<class T> requires (std::is_arithmetic_v<T>&&!std::is_same_v<T,char>&&!std::is_same_v<T,const char>)
+        int AddObject(struct VUIElement &target,T source,const char* HEAD){
+
+            return SUCCEDD;
+        }
+
+        /*Used to add a string as target's children,types like wchar_t,char,string,wstring are supported*/
+        template<class T> requires (std::is_convertible_v<T,std::string>&&(!std::is_arithmetic_v<T>||std::is_same_v<T,char>))
+        int AddObject(struct VUIElement &target,T source,const char* HEAD){
+
+            return SUCCEDD;
         }
     };
+    
     struct VUIArrayElement:VUIBasicElement{
         std::vector<VUIBasicElement> BODY;
         std::string OutSelf() override{
             std::string temp;
-            for (int i = 0; i < BODY.size(); i++)
+            for (auto i:std::views::iota(0,BODY.size()))
             {
                 temp.append(BODY.at(i).OutSelf());
                 if (i!=BODY.size()-2){
@@ -102,38 +136,6 @@ namespace vui{
 
     public:
         VUICreater(){}
-        /*Used to add a object as target's children*/
-        int AddObject(struct VUIElement &target,struct VUIElement source){
-            target.Children.push_back(source);
-            return SUCCEDD;
-        }
-        /*Used to add a number as target's children*/
-        
-        template<class T> requires (std::is_arithmetic_v<T>&&!std::is_same_v<T,char>&&!std::is_same_v<T,const char>)
-        int AddObject(struct VUIElement &target,T source,const char* HEAD){
-            struct VUIElement temp;
-            
-            temp.selfType=VUITypes::Number;
-            temp.HEAD=std::string(HEAD);
-            temp.Children.push_back(source);
-            target.Children.push_back(temp);
-            return SUCCEDD;
-        }
-        /*Used to add a string as target's children,types like wchar_t,char,string,wstring are supported*/
-        template<class T> requires (std::is_convertible_v<T,std::string>&&(!std::is_arithmetic_v<T>||std::is_same_v<T,char>))
-        int AddObject(struct VUIElement &target,T source,const char* HEAD){
-            struct VUIElement temp;
-            temp.selfType=VUITypes::Str;
-            temp.HEAD=std::string(HEAD);
-            temp.Children.push_back(source);//TODO:here is a bug
-            target.Children.push_back(temp);
-            return SUCCEDD;
-        }
-        /*
-        std::string OutVUI(struct VUIElement target){
-            for(auto i:target.Children){
-                
-            }
-        }*/
+    
     };
 }
